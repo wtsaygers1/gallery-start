45387d99186e11dced57c77e229f0e840aeeac0f
wtsaygers1 <wtsaygers1@gmail.com>
2021-02-15 13:28:26 -0700

    adds onclick handler

--- main.js
+++ main.js
@@ -9,5 +9,8 @@ const overlay = document.querySelector('.overlay');
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
+newImage.onclick = function(p) {
+    displayedImage.src = p.target.src;
+}
 
 /* Wiring up the Darken/Lighten button */
